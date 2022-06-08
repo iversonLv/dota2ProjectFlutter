9fb3cf1ab79c9fc51ef003169ac6d3e3174253f2
iversonLv <iverson_3_3_3@126.com>
2022-06-08 15:00:47 +0800

    Build base on 8724297

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -14,15 +14,19 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluent_ui/assets/AcrylicNoise.png": "81f27726c45346351eca125bd062e9a7",
 "assets/packages/fluent_ui/fonts/FluentIcons.ttf": "1cd173aed13e298ab2663dd0924f6762",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-
-"index.html": "ca77e9cedad5a8c7caf10f0cd4da2e21",
-"/": "ca77e9cedad5a8c7caf10f0cd4da2e21",
-"main.dart.js": "cf288a66d1dc76a24aac692f8a246238",
+"index.html": "254326cb1869a4f9846d1aba6d248654",
+"/": "254326cb1869a4f9846d1aba6d248654",
+"main.dart.js": "710f441ebd6405f459c8036e79803822",
 "manifest.json": "8c4043700633f750d90b98fbf470c60c",
 "version.json": "e662ce3084e1e5629714f58abff833d0"
 };
