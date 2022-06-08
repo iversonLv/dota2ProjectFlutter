87242975f0c979460cb8c1ba39c6f0a9512eafc8
iversonLv <iverson_3_3_3@126.com>
2022-06-08 14:44:23 +0800

    Updated top header to narrow layout, give more space to future average block

--- lib/screens/player/player-main.dart
+++ lib/screens/player/player-main.dart
@@ -58,95 +58,85 @@ class _PlayerMainState extends State<PlayerMain> {
 
                 return Container(
                   padding: const EdgeInsets.fromLTRB(35, 25, 25, 25),
-                  child: Column(
+                  decoration: const BoxDecoration(
+                    boxShadow: [BoxShadow(blurRadius: 2, color: Color.fromRGBO(0, 0, 0, .2), spreadRadius: 1, offset: Offset(0.0, 0.75))],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Center(
-                        // player avatar
-                        child: PlayerAvatar(player: player),
-                      ),
-                      const SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                      PlayerAvatar(player: player),
+                      const SizedBox(width: 10,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           // player name and confirmed by
-                          Column(
+                          Row(
                             children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    player.profile?.name != null ? player.profile?.name as String : player.profile?.personaname as String,
-                                    style: TextStyle(
-                                    fontSize: 28,
-                                    color: Theme.of(context).primaryColor,
-                                    ),
-                                  ),
-                                ],
+                              Text(
+                                player.profile?.name != null ? player.profile?.name as String : player.profile?.personaname as String,
+                                style: TextStyle(
+                                fontSize: 26,
+                                color: Theme.of(context).primaryColor,
+                                ),
                               ),
+                            const SizedBox(width: 5,),
+                            // confirmed by
+                            player.profile?.name != null ? TooltipWrapper(
+                                message: 'Confirmed by ${player.profile?.name}',
+                                child: const Icon(
+                                  Icons.check_circle_outlined,
+                                  color: yellowColor,
+                                  size: 25.0,
+                                ),
+                              ) : const SizedBox(),
+                              const SizedBox(width: 5,),
+                              // country
+                              player.profile?.loccountrycode != null ?
+                              TooltipWrapper(
+                                message: player.profile?.loccountrycode as String,
+                                child: Image(
+                                height: 25,
+                                width: 25,
+                                image: NetworkImage(
+                                  'https://community.cloudflare.steamstatic.com/public/images/countryflags/${player.profile?.loccountrycode!.toLowerCase()}.gif',
+                                  scale: 1
+                                ),
+                              )) : const SizedBox(),                              
+                              const SizedBox(width: 5,),
+                              // PLUS
+                              player.profile?.plus as bool ? 
+                              const TooltipWrapper(
+                                message: 'Dota plus subscriber', 
+                                child: Image(
+                                  width: 25,
+                                  height: 25,
+                                  image: NetworkImage('https://www.opendota.com/assets/images/dota2/dota_plus_icon.png'),
+                                )
+                              )
+                                : const SizedBox(),
                             ],
                           ),
-                          const SizedBox(width: 10,),
-                          // confirmed by
-                          player.profile?.name != null ? TooltipWrapper(
-                            message: 'Confirmed by ${player.profile?.name}',
-                            child: const Icon(
-                              Icons.check_circle_outlined,
-                              color: yellowColor,
-                              size: 25.0,
-                            ),
-                          ) : const SizedBox(),
-                          const SizedBox(width: 10,),
-                          // country
-                          player.profile?.loccountrycode != null ?
-                          TooltipWrapper(
-                            message: player.profile?.loccountrycode as String,
-                            child: Image(
-                            height: 25,
-                            width: 25,
-                            image: NetworkImage(
-                              'https://community.cloudflare.steamstatic.com/public/images/countryflags/${player.profile?.loccountrycode!.toLowerCase()}.gif',
-                              scale: 1
-                            ),
-                          )) : const SizedBox(),
+                          const SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // player wins stat
+                              PlayerWLstat(playerWl: playerWl, label: 'WINS', stat: playerWl.win.toString(), statColor: greenColor),
+                              const SizedBox(width: 25,),
+                              // player loses stat
+                              PlayerWLstat(playerWl: playerWl, label: 'LOSSES', stat: playerWl.lose.toString(), statColor: redColor),
+                              const SizedBox(width: 25,),
+                              // plaery winrate stat
+                              PlayerWLstat(playerWl: playerWl, label: 'WINRATE', stat: '${(playerWl.win! / (playerWl.lose! + playerWl.win!) * 100).toStringAsFixed(2)} %', statColor: Theme.of(context).primaryColor),
+                            ],
+                          ),
                         ],
                       ),
-                      const SizedBox(height: 15,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          // player wins stat
-                          PlayerWLstat(playerWl: playerWl, label: 'WINS', stat: playerWl.win.toString(), statColor: greenColor),
-                          const SizedBox(width: 25,),
-                          // player loses stat
-                          PlayerWLstat(playerWl: playerWl, label: 'LOSSES', stat: playerWl.lose.toString(), statColor: redColor),
-                          const SizedBox(width: 25,),
-                          // plaery winrate stat
-                          PlayerWLstat(playerWl: playerWl, label: 'WINRATE', stat: '${(playerWl.win! / (playerWl.lose! + playerWl.win!) * 100).toStringAsFixed(2)} %', statColor: Theme.of(context).primaryColor),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                        // PLUS
-                          player.profile?.plus as bool ? 
-                          const TooltipWrapper(
-                            message: 'Dota plus subscriber', 
-                            child: Image(
-                              width: 65,
-                              height: 75,
-                              image: NetworkImage('https://www.opendota.com/assets/images/dota2/dota_plus_icon.png'),
-                            )
-                          )
-                            : const SizedBox(),
-                          const SizedBox(width: 20,),
-                          // RankTierIcon
-                          RankTierIcon(player: player),
-                          const SizedBox(width: 20,),
-                        ],
-
-                      )
+                      const Spacer(),
+                      // RankTierIcon
+                      RankTierIcon(player: player),
                     ],
                   ),
                 );
